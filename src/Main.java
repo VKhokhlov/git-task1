4c5135f9f27e41f6dd8593495f563d8576cc28cd
Vladimir Khokhlov <vladimirkhokhlov@yandex.ru>
2022-07-22 17:12:49 +0700

    +SalesManager.stat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[]{1, 2, 3, 4, 5});
         System.out.println("max: " + salesManager.max());
+        System.out.println("stat: " + salesManager.stat());
     }
 }
